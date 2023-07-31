d7c7f647109065da78f30f575b001dfcc01d6463
Daria Tupchii <dariatupchiy23@gmail.com>
2023-07-31 14:00:28 -0400

    Home Page organized

--- BooksWeb/Areas/Customer/Controllers/HomeController.cs
+++ BooksWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Books.Models;
+﻿using Books.DataAccess.Repository.IRepository;
+using Books.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,31 @@ namespace BooksWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
+            return View(productList);
+        }
+
+        public IActionResult Details(int? productId)
+        {
+            if (productId is null || productId == 0)
+                return NotFound();
+
+            var product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
+            if (product == null) 
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         public IActionResult Privacy()
