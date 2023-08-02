613fd19da5aa1941c1d9a7b8f6a2237464051d93
Daria Tupchii <dariatupchiy23@gmail.com>
2023-08-02 11:35:08 -0400

    Identity Added

--- Books.DataAccess/Data/ApplicationDBContext.cs
+++ Books.DataAccess/Data/ApplicationDBContext.cs
@@ -1,11 +1,13 @@
 ﻿
 using Books.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
 
 namespace Books.DataAccess.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) 
         {
@@ -14,8 +16,11 @@ namespace Books.DataAccess.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating (ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
