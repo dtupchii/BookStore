93774a452ca7fec33c42d8606cef47fa319cb699
Daria Tupchii <dariatupchiy23@gmail.com>
2023-08-04 13:46:09 -0400

    ApplicationUser Repository added

---
+++ Books.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Books.DataAccess.Data;
+using Books.DataAccess.Repository.IRepository;
+using Books.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.DataAccess.Repository
+{
+    public class ApplicationUserRepository :  Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDBContext _db;
+        public ApplicationUserRepository(ApplicationDBContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
