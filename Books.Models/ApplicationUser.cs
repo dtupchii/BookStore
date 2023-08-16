2e010dcb1329d91ea2cc3cf1e4d8e4eccfc4681c
Daria Tupchii <dariatupchiy23@gmail.com>
2023-08-16 14:30:55 -0400

    User Management added

--- Books.Models/ApplicationUser.cs
+++ Books.Models/ApplicationUser.cs
@@ -23,6 +23,8 @@ namespace Books.Models
         public int? CompanyId { get; set; }
         [ForeignKey(nameof(CompanyId))]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
---
+++ BooksWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Books.DataAccess.Data;
+using Books.DataAccess.Repository.IRepository;
+using Books.Models;
+using Books.Models.ViewModels;
+using Books.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BooksWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public UserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {            
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> applicationUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in applicationUserList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
+            return Json(new { data = applicationUserList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = true, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Action completed successfully" });
+        }
+        #endregion
+
+    }
+}
